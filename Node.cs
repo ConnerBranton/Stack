18e3824769145459d4f1b2825424039c89adcc7c
ConnerBranton <106271358+ConnerBranton@users.noreply.github.com>
2022-06-23 11:05:17 -0500

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,25 @@
+﻿
+class Node
+{
+    private object _data;
+    private Node _next;
+
+    public object Data
+    {
+        get { return _data; }
+        set { _data = value; }
+    }//end property
+
+    public Node Next
+    {
+        get { return _next; }
+        set { _next = value; }
+    }//end property
+
+    public Node(object initialData = null)
+    {
+        _data = initialData;
+    }//end constructor
+
+
+}//end class
